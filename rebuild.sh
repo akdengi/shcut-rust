9a45714078940318cea66461381aa9c05371c4b6
akdengi <kazancas@gmail.com>
2026-08-16 22:11:20 +0400

    Comment out cleanup in rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Clean up after interrupted builds
-docker ps -aq | xargs -r docker stop
-docker system prune -f
-pkill -f cargo 2>/dev/null
-pkill -f rustc 2>/dev/null
-pkill -f npm 2>/dev/null
-sync
+# docker ps -aq | xargs -r docker stop
+# docker system prune -f
+# pkill -f cargo 2>/dev/null
+# pkill -f rustc 2>/dev/null
+# pkill -f npm 2>/dev/null
+# sync
 # echo 3 > /proc/sys/vm/drop_caches
 
 # Rebuild
